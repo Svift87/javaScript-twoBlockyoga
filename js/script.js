1e658d6e3b6a9271a98b42621eff2d52c7710e86
Svift <kuryakov-s@mail.ru>
2019-04-19 22:21:07 +0700

    ДЖоработал попапы и попрактиковался с созданием классов

--- js/script.js
+++ js/script.js
@@ -104,13 +104,15 @@ window.addEventListener('DOMContentLoaded', function () {
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
         popupClose = document.querySelector('.popup-close'),
-        descriptionBtn = document.querySelector('.description-btn');
-
-    descriptionBtn.addEventListener('click', function () {
-        overlay.style.display = 'block';
-        this.classList.add('more-splash');
-        document.body.style.overflow = 'hidden';
-    });
+        descriptionBtn = document.querySelectorAll('.description-btn');
+    for (let i = 0; i < descriptionBtn.length; i++) {
+        descriptionBtn[i].addEventListener('click', function () {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    }
+    
 
     more.addEventListener('click', function () {
         overlay.style.display = 'block';
